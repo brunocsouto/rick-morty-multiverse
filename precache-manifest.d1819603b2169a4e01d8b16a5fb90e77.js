efafcbb9ec7fdfb942b899da17dd7464179fb167
Bruno Souto <bruno.csouto@gmail.com>
2019-11-18 23:18:03 -0300

    Updates

---
+++ precache-manifest.d1819603b2169a4e01d8b16a5fb90e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc3c5d94e9dc58413bfa8e3b92e4de3",
+    "url": "/rick-morty-multiverse/index.html"
+  },
+  {
+    "revision": "3d75c005a7409c424bf5",
+    "url": "/rick-morty-multiverse/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "33e8b02161b5db2831b3",
+    "url": "/rick-morty-multiverse/static/css/main.cbed576c.chunk.css"
+  },
+  {
+    "revision": "3d75c005a7409c424bf5",
+    "url": "/rick-morty-multiverse/static/js/2.790aad35.chunk.js"
+  },
+  {
+    "revision": "33e8b02161b5db2831b3",
+    "url": "/rick-morty-multiverse/static/js/main.28b94269.chunk.js"
+  },
+  {
+    "revision": "3c9d297e7e3924edbee3",
+    "url": "/rick-morty-multiverse/static/js/runtime-main.c93d8558.js"
+  }
+]);
