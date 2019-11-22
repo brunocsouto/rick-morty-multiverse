eea6dd1fe51231350b7bec827dc8af2634e7a8bb
Bruno Souto <bruno.csouto@gmail.com>
2019-11-21 21:22:44 -0300

    Fixing Router relative path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Switch,
   Route,
   Link
@@ -11,15 +15,31 @@ import Characters from './pages/characters/index';
 import Episodes from './pages/episodes/index';
 import Locations from './pages/locations/index';
 import About from './pages/about/index';
-import Header from './components/Header/index';
+// import Header from './components/Header/index';
 
 function App() {
   return (
     <div className="App">
-        <Header />
-
+      <Router basename="/">
+        {/* <Header /> */}
+        <header>
+            <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar">
+                <Navbar.Brand>Multiverse</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Link to="/" className="nav-link">Home</Link>
+                        <Link to="/characters" className="nav-link">Characters</Link>
+                        <Link to="/episodes" className="nav-link">Episodes</Link>
+                        <Link to="/locations" className="nav-link">Locations</Link>
+                    </Nav>
+                    <Nav>
+                        <Link to="/about" className="nav-link">About</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </header>
         <div  className="container text-light">
-          <Router >
             <Switch>
               <Route path="/characters">
                 <Characters />
@@ -37,8 +57,8 @@ function App() {
                 <Home />
               </Route>
             </Switch>
-          </Router>
         </div>
+      </Router>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,17 +8,17 @@ function Header () {
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar">
-                <Navbar.Brand href="./">Multiverse</Navbar.Brand>
+                <Navbar.Brand href="/">Multiverse</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="./">Home</Nav.Link>
-                        <Nav.Link href="./characters">Characters</Nav.Link>
-                        <Nav.Link href="./episodes">Episodes</Nav.Link>
-                        <Nav.Link href="./locations">Locations</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/characters">Characters</Nav.Link>
+                        <Nav.Link href="/episodes">Episodes</Nav.Link>
+                        <Nav.Link href="/locations">Locations</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="./about">About</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
